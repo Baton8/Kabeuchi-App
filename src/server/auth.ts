a28b347aa2b90c321c35e78cb4c2845ca04e9890
Watakumi <watauta22@gmail.com>
2024-05-01 04:43:06 +0900

    Update environment variables and authentication providers

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,8 +5,7 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
-import DiscordProvider from "next-auth/providers/discord";
-import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import { env } from "@/env";
 import { db } from "@/server/db";
 
@@ -45,53 +44,22 @@ export const authOptions: NextAuthOptions = {
         id: user.id,
       },
     }),
+    signIn: async ({ user }) => {
+      if (!user.email) return false;
+      const isAllowedToSignIn = env.ALLOWED_SIGNIN_USERS.split(",").includes(
+        user.email
+      );
+      if (isAllowedToSignIn) {
+        return true;
+      }
+      return false;
+    },
   },
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        // Add logic here to look up the user from the credentials supplied
-        const users = [
-          {
-            id: "1",
-            name: "Admin User",
-            email: "admin@example.com",
-            username: "admin",
-            password: "password",
-          },
-        ];
-
-        // if credentials is undefined, return null
-        if (!credentials) {
-          return null;
-        }
-
-        const user = users.find(
-          (user) =>
-            user.username === credentials.username &&
-            user.password === credentials.password
-        );
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      },
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
     /**
      * ...add more providers here.
