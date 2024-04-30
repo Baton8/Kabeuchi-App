4c3ecb1d59829e04a2c95e900bf7c3eb56bd7c4e
Watakumi <watauta22@gmail.com>
2024-05-01 05:28:10 +0900

    Add OpenAI integration for chat functionality

---
+++ src/server/openai/chat.ts
@@ -0,0 +1,22 @@
+import { env } from "@/env";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: env.OPENAI_API_KEY,
+});
+
+type Messages = OpenAI.Chat.Completions.ChatCompletionMessageParam[];
+
+export async function createChat(messages: Messages): Promise<string> {
+  const res = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages,
+    temperature: 1,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  return res.choices[0]?.message?.content ?? "";
+}
