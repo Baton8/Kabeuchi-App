9733f34c9dd983db339d5694c76ed31d4415898e
Watakumi <watauta22@gmail.com>
2024-05-02 18:20:17 +0900

    Move components

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,7 @@
 import { Divider } from "@nextui-org/react";
 import { AttributesModal } from "./_components/attributes-modal";
 import { AttributeSidebar } from "./_components/attributes-sidebar";
-import { ChatContainer } from "./_components/chat-container";
+import { ChatContainer } from "./_components/messages/chat-container";
 
 export default function Page() {
   return (
