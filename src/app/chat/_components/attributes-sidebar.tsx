a8bcd10f798d700a2ffc139597b73d6bc28063d6
Watakumi <watauta22@gmail.com>
2024-05-02 18:12:21 +0900

    Add Divider and Add Conversation button to attributes sidebar

--- src/app/chat/_components/attributes-sidebar.tsx
+++ src/app/chat/_components/attributes-sidebar.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { Button } from "@nextui-org/react";
+import { Button, Divider } from "@nextui-org/react";
 import { TbPrompt } from "react-icons/tb";
 import { TbSettings } from "react-icons/tb";
 import { TbHistory } from "react-icons/tb";
+import { MdOutlineAdd } from "react-icons/md";
 import { useAttributesModal } from "./attributes-modal";
 import Image from "next/image";
 import Logo from "@/assets/images/wallbouncing.png";
@@ -11,9 +12,13 @@ export const AttributeSidebar = () => {
   const { handleModalOpen } = useAttributesModal();
   return (
     <aside className="flex flex-col justify-between items-center w-16 gap-4 h-screen">
-      <div className="mt-4">
+      <div className="mt-4 flex flex-col justify-center items-center w-full gap-2">
         <Button isIconOnly aria-label="WallBouncing" variant="bordered">
           <Image src={Logo} alt="WallBouncing" />
+        </Button>
+        <Divider className="w-[80%]" />
+        <Button isIconOnly color="primary" aria-label="Add Conversation">
+          <MdOutlineAdd className="text-xl" />
         </Button>
       </div>
 
