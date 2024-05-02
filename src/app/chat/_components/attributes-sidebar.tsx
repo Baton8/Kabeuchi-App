447ec4e0dc4e2bf6d9fc2a47f0e41b52ed09ea12
Watakumi <watauta22@gmail.com>
2024-05-03 08:41:18 +0900

    Add tooltips to buttons in attributes sidebar

--- src/app/chat/_components/attributes-sidebar.tsx
+++ src/app/chat/_components/attributes-sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Divider } from "@nextui-org/react";
+import { Button, Divider, Tooltip } from "@nextui-org/react";
 import { TbPrompt } from "react-icons/tb";
 import { TbSettings } from "react-icons/tb";
 import { TbHistory } from "react-icons/tb";
@@ -19,40 +19,52 @@ export const AttributeSidebar = () => {
   return (
     <aside className="flex flex-col justify-between items-center w-16 gap-4 h-screen">
       <div className="mt-4 flex flex-col justify-center items-center w-full gap-2">
-        <Button isIconOnly aria-label="WallBouncing" variant="bordered">
-          <Image src={Logo} alt="WallBouncing" />
-        </Button>
+        <Tooltip content="Top" placement="right" color="primary">
+          <Button isIconOnly aria-label="WallBouncing" variant="bordered">
+            <Image src={Logo} alt="WallBouncing" />
+          </Button>
+        </Tooltip>
         <Divider className="w-[80%]" />
-        <Button isIconOnly color="primary" aria-label="Add Conversation">
-          <MdOutlineAdd className="text-xl" />
-        </Button>
+        <Tooltip content="Add Chat" placement="right" color="primary">
+          <Button isIconOnly color="primary" aria-label="Add Chat">
+            <MdOutlineAdd className="text-xl" />
+          </Button>
+        </Tooltip>
       </div>
 
       <div className="flex flex-col justify-center items-center w-full gap-4">
-        <Button
-          isIconOnly
-          color="primary"
-          aria-label="Prompt"
-          onPress={promptsModalOpen}
-        >
-          <TbPrompt className="text-xl" />
-        </Button>
-        <Button
-          isIconOnly
-          color="primary"
-          aria-label="Settings"
-          onPress={attributesModalOpen}
-        >
-          <TbSettings className="text-xl" />
-        </Button>
-        <Button
-          isIconOnly
-          color="primary"
-          aria-label="History"
-          onPress={conversationHistoriesModalOpen}
-        >
-          <TbHistory className="text-xl" />
-        </Button>
+        <Tooltip content="Prompt" placement="right" color="primary">
+          <Button
+            isIconOnly
+            color="primary"
+            aria-label="Prompt"
+            onPress={promptsModalOpen}
+          >
+            <TbPrompt className="text-xl" />
+          </Button>
+        </Tooltip>
+
+        <Tooltip content="Settings" placement="right" color="primary">
+          <Button
+            isIconOnly
+            color="primary"
+            aria-label="Settings"
+            onPress={attributesModalOpen}
+          >
+            <TbSettings className="text-xl" />
+          </Button>
+        </Tooltip>
+
+        <Tooltip content="History" placement="right" color="primary">
+          <Button
+            isIconOnly
+            color="primary"
+            aria-label="History"
+            onPress={conversationHistoriesModalOpen}
+          >
+            <TbHistory className="text-xl" />
+          </Button>
+        </Tooltip>
       </div>
       <div></div>
     </aside>
