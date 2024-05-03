362c99825162f136a0dbe43f2c4cbf21b0e44f85
Watakumi <watauta22@gmail.com>
2024-05-03 15:47:00 +0900

    Refactor age options in settings component

--- src/app/chat/_components/settings/age.tsx
+++ src/app/chat/_components/settings/age.tsx
@@ -2,38 +2,30 @@
 import { Select, SelectItem } from "@nextui-org/react";
 
 type Age = {
-  value: string;
-  label: string;
+  name: string;
 };
 
 const ages: Age[] = [
   {
-    value: "10代",
-    label: "10代",
+    name: "10代",
   },
   {
-    value: "20代",
-    label: "20代",
+    name: "20代",
   },
   {
-    value: "30代",
-    label: "30代",
+    name: "30代",
   },
   {
-    value: "40代",
-    label: "40代",
+    name: "40代",
   },
   {
-    value: "50代",
-    label: "50代",
+    name: "50代",
   },
   {
-    value: "60代",
-    label: "60代",
+    name: "60代",
   },
   {
-    value: "70代",
-    label: "70代",
+    name: "70代",
   },
 ];
 export const Age = () => {
@@ -41,8 +33,8 @@ export const Age = () => {
     <div>
       <Select label="年齢" className="max-w-xs">
         {ages.map((age) => (
-          <SelectItem key={age.value} value={age.value}>
-            {age.label}
+          <SelectItem key={age.name} value={age.name}>
+            {age.name}
           </SelectItem>
         ))}
       </Select>
