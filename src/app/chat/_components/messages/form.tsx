6d87444a8763c810af3430ea32d478690c765fee
Watakumi <watauta22@gmail.com>
2024-05-03 09:47:50 +0900

    Add form submission functionality to chat message form

--- src/app/chat/_components/messages/form.tsx
+++ src/app/chat/_components/messages/form.tsx
@@ -1,11 +1,31 @@
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Button, Textarea } from "@nextui-org/react";
 import { FC } from "react";
 
+type Inputs = {
+  message: string;
+};
+
 export const Form: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form className="w-full flex flex-row items-center gap-4 px-4 pb-8">
-      <Textarea></Textarea>
-      <Button color="primary">送信</Button>
+    <form
+      className="w-full flex flex-row items-center gap-4 px-4 pb-8"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <Textarea {...register("message", { required: true })}></Textarea>
+      <Button color="primary" type="submit">
+        送信
+      </Button>
     </form>
   );
 };
