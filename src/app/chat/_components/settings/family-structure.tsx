07260f76756e0ebb3fb0e71ebc7e27cfa5fe82bc
Watakumi <watauta22@gmail.com>
2024-05-05 11:42:36 +0900

    Refactor family structure options in settings component and useAttributes hook

--- src/app/chat/_components/settings/family-structure.tsx
+++ src/app/chat/_components/settings/family-structure.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Select, SelectItem } from "@nextui-org/react";
+import { useState, useEffect, type ChangeEventHandler } from "react";
+import { useAttributes } from "../../_hooks/use-attributes";
 
 type FamilyStructure = {
   name: string;
@@ -14,9 +16,33 @@ const familyStructures: FamilyStructure[] = [
   { name: "単親家庭" },
 ];
 export const FamilyStructure = () => {
+  const [value, setValue] = useState<string>("");
+
+  const { attributes, setAttributes } = useAttributes();
+
+  const defaultFamilyStructure = familyStructures.find(
+    (familyStructure) => familyStructure.name === attributes.familyStructure
+  );
+
+  useEffect(() => {
+    if (defaultFamilyStructure) {
+      setValue(defaultFamilyStructure.name);
+    }
+  }, [defaultFamilyStructure]);
+
+  const handleChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    setValue(e.target.value);
+    setAttributes({ ...attributes, familyStructure: e.target.value });
+  };
+
   return (
     <div>
-      <Select label="家族構成" className="max-w-xs">
+      <Select
+        label="家族構成"
+        className="max-w-xs"
+        selectedKeys={[value]}
+        onChange={handleChange}
+      >
         {familyStructures.map((familyStructure) => (
           <SelectItem key={familyStructure.name} value={familyStructure.name}>
             {familyStructure.name}
