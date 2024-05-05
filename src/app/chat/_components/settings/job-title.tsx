051a6398c7b9e48e48e46c739d050b5a174b34bf
Watakumi <watauta22@gmail.com>
2024-05-05 11:38:26 +0900

    Refactor job title options in settings component and useAttributes hook

--- src/app/chat/_components/settings/job-title.tsx
+++ src/app/chat/_components/settings/job-title.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Select, SelectItem } from "@nextui-org/react";
+import { useEffect, useState, type ChangeEventHandler } from "react";
+import { useAttributes } from "../../_hooks/use-attributes";
 
 type JobTitle = {
   name: string;
@@ -22,9 +24,33 @@ const jobTitles: JobTitle[] = [
   { name: "ウェディングプランナー" },
 ];
 export const JobTitle = () => {
+  const [value, setValue] = useState<string>("");
+
+  const { attributes, setAttributes } = useAttributes();
+
+  const defaultJobTitle = jobTitles.find(
+    (jobTitle) => jobTitle.name === attributes.jobTitle
+  );
+
+  useEffect(() => {
+    if (defaultJobTitle) {
+      setValue(defaultJobTitle.name);
+    }
+  }, [defaultJobTitle]);
+
+  const handleChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    setValue(e.target.value);
+    setAttributes({ ...attributes, jobTitle: e.target.value });
+  };
+
   return (
     <div>
-      <Select label="職種" className="max-w-xs">
+      <Select
+        label="職種"
+        className="max-w-xs"
+        selectedKeys={[value]}
+        onChange={handleChange}
+      >
         {jobTitles.map((jt) => (
           <SelectItem key={jt.name} value={jt.name}>
             {jt.name}
