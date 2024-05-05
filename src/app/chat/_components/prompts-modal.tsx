4e36a37cf71e014812a12d60e4810869f8db6b6b
Watakumi <watauta22@gmail.com>
2024-05-05 19:52:53 +0900

    Refactor PromptsModal component to use Prompt component in prompts-modal.tsx

--- src/app/chat/_components/prompts-modal.tsx
+++ src/app/chat/_components/prompts-modal.tsx
@@ -1,25 +1,101 @@
 "use client";
 import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
   Button,
+  ButtonGroup,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Textarea,
 } from "@nextui-org/react";
 import { atom, useAtomValue, useSetAtom } from "jotai";
-import type { FC } from "react";
+import { useRef, type FC } from "react";
+import { usePrompt } from "../_hooks/use-prompt";
 
-type Prompt = {
-  text: string;
-};
+const replacedText = {
+  age: "${age}",
+  gender: "${gender}",
+  education: "${education}",
+  jobHistory: "${jobHistory}",
+  jobTitle: "${jobTitle}",
+  jobPosition: "${jobPosition}",
+  communicationStyle: "${communicationStyle}",
+  residence: "${residence}",
+  income: "${income}",
+  skillSet: "${skillSet}",
+} as const;
 
-type PromptProps = {
-  prompt: Prompt;
-};
+export const Prompt: FC = () => {
+  const { prompt, setPrompt, resetPrompt } = usePrompt();
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-export const Prompt: FC<PromptProps> = ({ prompt }) => {
-  return <div>{prompt.text}</div>;
+  const insertTextAtCursor = (insertText: string) => {
+    if (textareaRef.current) {
+      const textarea = textareaRef.current;
+      const start = textarea.selectionStart;
+      const end = textarea.selectionEnd;
+      const newText =
+        prompt.substring(0, start) + insertText + prompt.substring(end);
+      setPrompt(newText);
+
+      setTimeout(() => {
+        textarea.selectionStart = textarea.selectionEnd =
+          start + insertText.length;
+        textarea.focus();
+      }, 0);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2 justify-center items-start">
+        <p>変数</p>
+        <ButtonGroup>
+          <Button onPress={() => insertTextAtCursor(replacedText.gender)}>
+            性別
+          </Button>
+          <Button onPress={() => insertTextAtCursor(replacedText.age)}>
+            年齢
+          </Button>
+          <Button onPress={() => insertTextAtCursor(replacedText.education)}>
+            学歴
+          </Button>
+          <Button onPress={() => insertTextAtCursor(replacedText.residence)}>
+            居住地域
+          </Button>
+          <Button
+            onPress={() => insertTextAtCursor(replacedText.communicationStyle)}
+          >
+            コミュニケーションスタイル
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button onPress={() => insertTextAtCursor(replacedText.jobTitle)}>
+            職種
+          </Button>
+          <Button onPress={() => insertTextAtCursor(replacedText.jobHistory)}>
+            職歴
+          </Button>
+          <Button onPress={() => insertTextAtCursor(replacedText.jobPosition)}>
+            役職
+          </Button>
+          <Button onPress={() => insertTextAtCursor(replacedText.income)}>
+            所得
+          </Button>
+          <Button onPress={() => insertTextAtCursor(replacedText.skillSet)}>
+            スキルセット
+          </Button>
+        </ButtonGroup>
+      </div>
+      <Textarea value={prompt} ref={textareaRef} onValueChange={setPrompt} />
+      <div>
+        <Button onClick={resetPrompt} color="warning">
+          リセット
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 const promptsModalAtom = atom(false);
@@ -51,7 +127,7 @@ export const PromptsModal: FC = () => {
               プロンプト
             </ModalHeader>
             <ModalBody>
-              <Prompt prompt={{ text: "プロンプト" }} />
+              <Prompt />
             </ModalBody>
             <ModalFooter>
               <Button color="primary" onPress={onClose}>
