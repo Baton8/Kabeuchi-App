d468b345f127d35f6672d10817f65c1c43528a90
Watakumi <watauta22@gmail.com>
2024-05-05 16:54:02 +0900

    Refactor skill set options in settings component and useAttributes hook

--- src/app/chat/_components/settings/skill-set.tsx
+++ src/app/chat/_components/settings/skill-set.tsx
@@ -1,9 +1,30 @@
 import { Textarea } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import { useAttributes } from "../../_hooks/use-attributes";
 
 export const SkillSet = () => {
+  const [value, setValue] = useState<string>("");
+  const { attributes, setAttributes } = useAttributes();
+
+  useEffect(() => {
+    if (attributes.skillSet) {
+      setValue(attributes.skillSet);
+    }
+  }, [attributes.skillSet]);
+
+  const handleChange = (value: string) => {
+    setValue(value);
+    setAttributes({ ...attributes, skillSet: value });
+  };
+
   return (
     <div>
-      <Textarea label="スキルセット" className="max-w-xs" />
+      <Textarea
+        label="スキルセット"
+        className="max-w-xs"
+        value={value}
+        onValueChange={handleChange}
+      />
     </div>
   );
 };
