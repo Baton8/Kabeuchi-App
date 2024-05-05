50396a200edd31636a74ada260c101bd2cabaabf
Watakumi <watauta22@gmail.com>
2024-05-05 11:16:03 +0900

    Refactor age options in settings component and useAttributes hook

--- src/app/chat/_components/settings/age.tsx
+++ src/app/chat/_components/settings/age.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { Select, SelectItem } from "@nextui-org/react";
+import { useEffect, useState, type ChangeEventHandler } from "react";
+import { useAttributes } from "../../_hooks/use-attributes";
 
 type Age = {
   name: string;
@@ -28,10 +30,35 @@ const ages: Age[] = [
     name: "70代",
   },
 ];
+
 export const Age = () => {
+  const [value, setValue] = useState<string>("");
+
+  const { attributes, setAttributes } = useAttributes();
+
+  const defaultAge = ages.find((age) => age.name === attributes.age);
+
+  useEffect(() => {
+    if (defaultAge) {
+      setValue(defaultAge.name);
+    }
+  }, [defaultAge]);
+
+  const handleChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    setValue(e.target.value);
+    setAttributes({ ...attributes, age: e.target.value });
+  };
+
+  console.log("attributes", attributes);
+
   return (
     <div>
-      <Select label="年齢" className="max-w-xs">
+      <Select
+        label="年齢"
+        className="max-w-xs"
+        selectedKeys={[value]}
+        onChange={handleChange}
+      >
         {ages.map((age) => (
           <SelectItem key={age.name} value={age.name}>
             {age.name}
