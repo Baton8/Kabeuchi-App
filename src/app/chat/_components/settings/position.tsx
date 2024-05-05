e4c8b29d0184848611c2a50f4571e14d26212264
Watakumi <watauta22@gmail.com>
2024-05-05 11:33:28 +0900

    Refactor job position options in settings component and useAttributes hook

--- src/app/chat/_components/settings/position.tsx
+++ src/app/chat/_components/settings/position.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { Select, SelectItem } from "@nextui-org/react";
+import { type ChangeEventHandler, useEffect, useState } from "react";
+import { useAttributes } from "../../_hooks/use-attributes";
 
 type Position = {
   name: string;
@@ -13,9 +15,33 @@ const positions: Position[] = [
   { name: "代表：最終意思決定者" },
 ];
 export const JobPosition = () => {
+  const [value, setValue] = useState<string>("");
+
+  const { attributes, setAttributes } = useAttributes();
+
+  const defaultJobPosition = positions.find(
+    (position) => position.name === attributes.jobPosition
+  );
+
+  useEffect(() => {
+    if (defaultJobPosition) {
+      setValue(defaultJobPosition.name);
+    }
+  }, [defaultJobPosition]);
+
+  const handleChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
+    setValue(e.target.value);
+    setAttributes({ ...attributes, jobPosition: e.target.value });
+  };
+
   return (
     <div>
-      <Select label="役職" className="max-w-xs">
+      <Select
+        label="役職"
+        className="max-w-xs"
+        selectedKeys={[value]}
+        onChange={handleChange}
+      >
         {positions.map((jt) => (
           <SelectItem key={jt.name} value={jt.name}>
             {jt.name}
